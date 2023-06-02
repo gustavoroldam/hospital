075bb4205f3b8086b06c9d32243dd1767add4fd0
GustavoThe <105975282+gustavoroldam@users.noreply.github.com>
2023-06-02 15:49:14 -0300

    ...

--- VIEW/menu.php
+++ VIEW/menu.php
@@ -21,46 +21,17 @@
 </head>
 
 <body>
-    <!-- Dropdown Structure -->
-    <ul id="medico" class="dropdown-content">
-        <li><a href="#!" class="light-blue-text">Detalhamento</a></li>
-        <li><a href="#!" class="light-blue-text">Alterar</a></li>
-        <li><a href="#!" class="light-blue-text">Cadastrar</a></li>
-        <li class="divider"></li>
-        <li><a href="\hospital\VIEW\Medico\lstMedico.php" class="light-blue-text">Lista</a></li>
-    </ul>
-    <ul id="paciente" class="dropdown-content">
-        <li><a href="#!" class="light-blue-text">Detalhamento</a></li>
-        <li><a href="#!" class="light-blue-text">Alterar</a></li>
-        <li><a href="#!" class="light-blue-text">Cadastrar</a></li>
-        <li class="divider"></li>
-        <li><a href="#!" class="light-blue-text">Lista</a></li>
-    </ul>
-    <ul id="medicamento" class="dropdown-content">
-        <li><a href="#!" class="light-blue-text">Detalhamento</a></li>
-        <li><a href="#!" class="light-blue-text">Alterar</a></li>
-        <li><a href="#!" class="light-blue-text">Cadastrar</a></li>
-        <li class="divider"></li>
-        <li><a href="\hospital\VIEW\Medicamento\lstMedicamento.php" class="light-blue-text">Lista</a></li>
-    </ul>
+
     <nav>
-        <div class="nav-wrapper light-blue darken-4">
-            <a href="#!" class="brand-logo"><img src="../imagens/logo_branco.jpg" alt="Girl in a jacket" width="200" height="64"></a>
-            <ul class="right hide-on-med-and-down">
-                <!-- Dropdown Trigger -->
-                <li><a class="dropdown-trigger" href="#!" data-target="medico">Medico<i class="material-icons right">arrow_drop_down</i></a></li>
-                <li><a class="dropdown-trigger" href="#!" data-target="paciente">Paciente<i class="material-icons right">arrow_drop_down</i></a></li>
-                <li><a class="dropdown-trigger" href="#!" data-target="medicamento">Medicamento<i class="material-icons right">arrow_drop_down</i></a></li>
-                <li><a href="\hospital\VIEW\logout.php">Sair</a></li>
-            </ul>
-        </div>
-    </nav>
+    <div class="nav-wrapper light-blue darken-4">
+    <a href="#!" class="brand-logo"><img src="../imagens/logo_branco.jpg" alt="Girl in a jacket" width="200" height="64"></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="\hospital\VIEW\Medico\lstMedico.php">Medico</a></li>
+        <li><a href="\hospital\VIEW\Paciente\lstPaciente.php">Paciente</a></li>
+        <li><a href="\hospital\VIEW\Medicamento\lstMedicamento.php">Medicamento</a></li>
+        <li><a href="\hospital\VIEW\logout.php">Sair</a></li>
+      </ul>
+    </div>
+  </nav>
 
 </html>
-
-<script>
-    const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
-    const instancesDropdown = M.Dropdown.init(elemsDropdown, {
-        coverTrigger: false
-    })
-</script>
